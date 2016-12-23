fe314f24daef98fbdddd7a0593601277da7e33e2
cdaringe <christopher.dieringer@gmail.com>
2016-12-22 17:02:01 -0800

    fix(build): publish docs w/ token

--- scripts/ci-post-test.js
+++ scripts/ci-post-test.js
@@ -1,13 +1,16 @@
 'use strict'
 
 const cp = require('child_process')
+const path = require('path')
+const ghPages = require('gh-pages')
 
 if (process.env.CI_BRANCH === 'master') {
-  cp.execSync([
-    'npm run semantic-release || true',
-    'npm run docs',
-    'npm run docs-publish'
-  ].join(' && '))
+  cp.execSync('npm run semantic-release || true')
+  cp.execSync('npm run docs')
+  ghPages.publish(path.join(__dirname, '..', 'docs'), {
+    repo: 'https://' + process.env.GH_TOKEN + '@github.com/Tripwire/octagon.git',
+    silent: true
+  }, () => console.log('docs-published!'))
 } else {
   console.warn('not on master, skipping publish cycle')
 }
