b890a683f40fda8b08fc8209e491e451bdbec1ef
cdaringe <christopher.dieringer@gmail.com>
2016-12-22 17:11:31 -0800

    fix(pkg): correct metadatas!

--- scripts/ci-post-test.js
+++ scripts/ci-post-test.js
@@ -4,9 +4,14 @@ const cp = require('child_process')
 const path = require('path')
 const ghPages = require('gh-pages')
 
+const CP_OPTS = { cwd: path.join(__dirname, '..') }
+
 if (process.env.CI_BRANCH === 'master') {
-  cp.execSync('npm run semantic-release || true')
-  cp.execSync('npm run docs')
+  console.log('running semantic release...')
+  cp.execSync('npm run semantic-release || true', CP_OPTS)
+  console.log('building storybook...')
+  cp.execSync('npm run docs', CP_OPTS)
+  console.log('publishing storybook...')
   ghPages.publish(path.join(__dirname, '..', 'docs'), {
     repo: 'https://' + process.env.GH_TOKEN + '@github.com/Tripwire/octagon.git',
     silent: true
