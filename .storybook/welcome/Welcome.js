e9c403b7dd5e775c9acf1e0ffe0916b12c801137
cdaringe <christopher.dieringer@gmail.com>
2017-01-03 11:01:16 -0700

    docs(readme): add sort story todo

--- .storybook/welcome/Welcome.js
+++ .storybook/welcome/Welcome.js
@@ -10,6 +10,10 @@ const md = `
 A storybook is a composition of many stories.  A story is a single state of one or more UI components. You can have as many stories as you want. Basically a story is like a visual test case.  Check out the stories inside the \`src/<component>/\` directories.
 
 You can also edit components and see changes right away.  This is just one thing you can do with Storybook.  Have a look at the react-storybook project for more information.
+
+## todo
+
+- [ ] [sort stories](https://github.com/storybooks/react-storybook/issues/548)
 `
 
 export default class Welcome extends React.Component {
