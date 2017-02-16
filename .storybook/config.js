0bcc133286242d1e892d5f3e31abe893413a99d7
Edward Raffaele <eraffaele@gmail.com>
2017-02-16 11:33:12 -0800

    switch to non min semantic css

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,5 @@
 // import '../lib/styles/semantic.css'
-import '../semantic/dist/semantic.min.css';
+import '../semantic/dist/semantic.css';
 import './styles.css'
 import { configure } from '@kadira/storybook'
 import { basename } from 'path'
