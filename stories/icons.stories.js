7e642185ceaa20547ea44327ba0d6dd12f43f542
Edward Raffaele <eraffaele@gmail.com>
2017-02-22 09:43:30 -0800

    feat(icons): add icons
    Enables access to the entire elegant icons font class using the namespace ei

--- stories/icons.stories.js
+++ stories/icons.stories.js
@@ -3,7 +3,6 @@ import { storiesOf } from '@kadira/storybook'
 import { Icon } from 'semantic-ui-react'
 import '../src/styles/app.css'
 
-
 storiesOf('Icons', module)
   .addDecorator((story) => (
     <div style={{width: '400px'}}>
@@ -11,146 +10,141 @@ storiesOf('Icons', module)
     </div>
   ))
     .add('Icons', () => (
+      <div>
         <div>
-            <div>
-                <span style={{marginRight: "20px"}}>
+          <span style={{marginRight: '20px'}}>
                     Warning
                 </span>
-                <Icon className="tw" name='warning' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
+          <Icon className='ei' name='icon_error-triangle_alt' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
                     Alert
                 </span>
-                <Icon className="tw" name='alert' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
+          <Icon className='ei' name='icon_error-circle_alt' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
                     Success
                 </span>
-                <Icon className="tw" name='success' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
+          <Icon className='ei' name='icon_check_alt2' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
                     Stopped
                 </span>
-                <Icon className="tw" name='stopped' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
+          <Icon className='ei' name='icon_minus_alt2' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
                     Settings
                 </span>
-                <Icon className="tw" name='settings' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
-                    Warning
+          <Icon className='ei' name='icon_cog' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
+                    Dialog Warning
                 </span>
-                <Icon className="tw" name='menu' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
-                    Error
+          <Icon className='ei' name='icon_error-triangle' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
+                   Dialog Error
                 </span>
-                <Icon className="tw" name='error' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
-                    Alt Warning
+          <Icon className='ei' name='icon_error-circle' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
+                    Dialog Success
                 </span>
-                <Icon className="tw" name='altwarning' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
-                    Alt Success
+          <Icon className='ei' name='icon_check_alt' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
+                    Dialog Info
                 </span>
-                <Icon className="tw" name='altsuccess' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
-                    Info
+          <Icon className='ei' name='icon_info' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
+                    Operation Run
                 </span>
-                <Icon className="tw" name='info' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
-                    Run
+          <Icon className='ei' name='arrow_triangle-right_alt2' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
+                    Operation Stop
                 </span>
-                <Icon className="tw" name='run' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
-                    Stop
-                </span>
-                <Icon className="tw" name='stop' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
+          <Icon className='ei' name='icon_pause_alt2' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
                     Chevron
                 </span>
-                <Icon className="tw" name='chevronright' />
-                <Icon className="tw" name='chevronleft' />
-                <Icon className="tw" name='chevronup' />
-                <Icon className="tw" name='chevrondown' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
-                    Arrow
+          <Icon className='ei' name='arrow_carrot-right' />
+          <Icon className='ei' name='arrow_carrot-left' />
+          <Icon className='ei' name='arrow_carrot-down' />
+          <Icon className='ei' name='arrow_carrot-up' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
+                    Trend Arrow
                 </span>
-                <Icon className="tw" name='arrowdown' />
-                <Icon className="tw" name='arrowup' />
-                <Icon className="tw" name='arrowleft' />
-                <Icon className="tw" name='arrowright' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
-                    Inverted Success
+          <Icon className='ei' name='arrow_triangle-right' />
+          <Icon className='ei' name='arrow_triangle-left' />
+          <Icon className='ei' name='arrow_triangle-down' />
+          <Icon className='ei' name='arrow_triangle-up' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
+                    Success Checkmark
                 </span>
-                <Icon className="tw" name='inversesuccess' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
+          <Icon className='ei' name='icon_check' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
                     Search
                 </span>
-                <Icon className="tw" name='search' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
+          <Icon className='ei' name='icon_search' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
                     User
                 </span>
-                <Icon className="tw" name='user' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
+          <Icon className='ei' name='icon_profile' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
                     Tag
                 </span>
-                <Icon className="tw" name='tag' />
-            </div>
-            <div>
-                <span style={{marginRight: "20px"}}>
+          <Icon className='ei' name='icon_tag_alt' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
                     Folder
                 </span>
-                <Icon className="tw" name='folder' />
-            </div>
+          <Icon className='ei' name='icon_folder-alt' />
+        </div>
 
-            <div>
-                <span style={{marginRight: "20px"}}>
+        <div>
+          <span style={{marginRight: '20px'}}>
                     Sizing
                 </span>
-                <Icon className="tw" name='folder' />
-                <Icon className="tw" name='folder' size="large" />
-                <Icon className="tw" name='folder' size="big" />
-                <Icon className="tw" name='folder' size="huge" />
-            </div>            
-            <div>
-                <span style={{marginRight: "20px"}}>
+          <Icon className='ei' name='icon_folder-alt' />
+          <Icon className='ei' name='icon_folder-alt' size='large' />
+          <Icon className='ei' name='icon_folder-alt' size='big' />
+          <Icon className='ei' name='icon_folder-alt' size='huge' />
+        </div>
+        <div>
+          <span style={{marginRight: '20px'}}>
                     Colors
                 </span>
-                <Icon className="tw" name='folder' color="red" size="big" />
-                <Icon className="tw" name='folder' color="grey" size="big" />
-                <Icon className="tw" name='folder' color="yellow" size="big" />
-            </div>   
+          <Icon className='ei' name='icon_folder-alt' color='red' size='big' />
+          <Icon className='ei' name='icon_folder-alt' color='grey' size='big' />
+          <Icon className='ei' name='icon_folder-alt' color='yellow' size='big' />
+        </div>
+
+            Note that these icons are namespaced with the class "ei" which enables you to access the Elegant Icons while retaining the usage of the Semantic UI Icon component.
         </div>
     ))
-
 
