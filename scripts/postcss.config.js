7536120e07d1794ffcb8a51d01406fb1e94b2128
Christopher Dieringer <cdaringe@users.noreply.github.com>
2017-03-03 13:02:43 -0800

    chore(pkg): add breadcrumbs

--- scripts/postcss.config.js
+++ scripts/postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('postcss-cssnext'),
-    require('postcss-neat'),
-    require('rucksack-css'),
+    require('postcss-cssnext'), // permit future css rules
+    require('postcss-neat'), // @TODO investigate purging? may be unused
+    require('rucksack-css'), // css function superset (e.g. less like syntax)
     require('autoprefixer')({ browsers: ['last 2 version'] })
   ]
 }
