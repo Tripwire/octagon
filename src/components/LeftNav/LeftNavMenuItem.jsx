7ccf9e3a3dfc49c54ec38fa2d31334001fdddd4c
Edward Raffaele <eraffaele@gmail.com>
2017-03-20 12:15:34 -0700

    wip(leftnav): initial work on left nav
    adds the initial work for left nav for preview

--- src/components/LeftNav/LeftNavMenuItem.jsx
+++ src/components/LeftNav/LeftNavMenuItem.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const LeftNavMenuItem = (props) => {
   let active = ''
+  let activeIcon = ''
   active = props.active ? 'active' : ''
+  activeIcon = props.active ? 'active-icon' : ''
   return (
     <li className='nav__item' onClick={props.onClick}>
       <div className={`icon_container ${active}`}>
         {(props.image)
-                    ? <img className='icon_nav' src={props.image} />
+                    ? <img className={`icon_nav ${activeIcon}`} src={props.image} />
                     : null}
         {props.children}
       </div>
