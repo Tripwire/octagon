7964da72bbb7f3f1fe737d2905915836ac83817b
Edward Raffaele <eraffaele@gmail.com>
2017-03-22 09:42:29 -0700

    feat(leftnav): add left nav
    adds the left nav component to the project

--- src/components/LeftNav/LeftNavMenuItem.jsx
+++ src/components/LeftNav/LeftNavMenuItem.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const LeftNavMenuItem = (props) => {
   let active = ''
   let activeIcon = ''
-  active = props.active ? 'active' : ''
+  active = props.active ? 'icon_container-active' : 'icon_container-inactive'
   activeIcon = props.active ? 'active-icon' : ''
   return (
     <li className='nav__item' onClick={props.onClick}>
       <div className={`icon_container ${active}`}>
         {(props.image)
-                    ? <img className={`icon_nav ${activeIcon}`} src={props.image} />
-                    : null}
+          ? <img className={`icon_nav ${activeIcon}`} src={props.image} />
+          : null}
         {props.children}
       </div>
     </li>
