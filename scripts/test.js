da08ee826c6ad0d2f80cd57c21c883030c51d14b
cdaringe <christopher.dieringer@gmail.com>
2017-03-27 15:26:56 -0700

    fix(test): boot backstop slowly s.t. it GET/s the static site post boot

--- scripts/test.js
+++ scripts/test.js
@@ -9,22 +9,24 @@ const npmBin = path.resolve(projectRoot, 'node_modules', '.bin')
 
 let processHandles = []
 function kill () {
-  processHandles.forEach(handle => {
-    try {
-      handle.kill()
-    } catch (err) {
-      // pass
-    }
-  })
+  processHandles.forEach(handle => { try { handle.kill() } catch (err) {} })
 }
 
 const staticContentDir = path.join(projectRoot, 'styleguide')
 const server = cp.spawn('httpster', ['-d', staticContentDir], { cwd: npmBin, stdio: 'inherit' })
-processHandles.push(server.childProcess)
+processHandles.push(server)
+server.on('exit', code => {
+  if (code) throw new Error('httpster unable to serve')
+  kill()
+})
 
 const backstopBin = path.join(npmBin, 'backstop')
-const backstop = cp.spawn(backstopBin, ['test'], { cwd: projectRoot, stdio: 'inherit' })
-processHandles.push(backstop.childProcess)
-backstop.on('exit', code => process.exit(code))
+let backstop
+
+setTimeout(() => {
+  backstop = cp.spawn(backstopBin, ['test'], { cwd: projectRoot, stdio: 'inherit' })
+  processHandles.push(backstop.childProcess)
+  backstop.on('exit', code => process.exit(code))
+}, 5000)
 
 process.on('exit', kill)
