ff452ae6fc29cd72f156bf84eea2a592b1b670ac
cdaringe <christopher.dieringer@gmail.com>
2017-10-27 15:37:39 -0700

    feat(build): parallel build steps

--- scripts/builder.js
+++ scripts/builder.js
@@ -3,10 +3,8 @@
  */
 'use strict'
 
-const cp = require('child_process')
 const fs = require('fs-extra')
 const path = require('path')
-const pify = require('pify')
 const os = require('os')
 const execa = require('execa')
 const bb = require('bluebird')
@@ -14,12 +12,7 @@ const folderHash = require('folder-hash')
 const toilet = require('toiletdb')
 const db = bb.promisifyAll(toilet(path.resolve(__dirname, 'cache.json')))
 const less = require('less')
-const link = require('fs-symlink')
 
-const exec = pify(cp.exec, { multiArgs: true })
-const copy = pify(fs.copy)
-const mkdirp = pify(fs.mkdirp)
-const remove = pify(fs.remove)
 const isWin = os.platform().match(/^win/)
 
 module.exports = {
@@ -27,7 +20,7 @@ module.exports = {
   get coverageDir () { return path.join(this.projectRoot, 'coverage') },
   get distDir () { return path.join(this.projectRoot, 'lib') },
   get projectRoot () { return path.resolve(__dirname, '..') },
-  get tempBuildDir () { return path.join('semanticTempBuildDir') },
+  get tempBuildDir () { return path.join(this.projectRoot, 'semanticTempBuildDir') },
   get semanticUiLessPath () { return path.join(this.projectRoot, 'node_modules/semantic-ui-less') },
   get srcPath () { return path.join(this.projectRoot, 'src') },
   get postCssConfig () { return path.join(this.projectRoot, 'postcss.config.js') },
@@ -41,18 +34,25 @@ module.exports = {
    */
   async build () {
     await this.clean()
-    await this.octagonComponentJs()
-    await this.octagonComponentCss()
-    await this.octagonCopyAssets()
-    await this.semanticInit()
-    console.log('Lib built successfully')
+    await fs.mkdirp(this.distDir)
+    return Promise.all([
+      this.buildOctagonNative(),
+      this.semanticInit()
+    ])
+  },
+  async buildOctagonNative () {
+    return Promise.all([
+      this.octagonComponentJs(),
+      this.octagonComponentCss(),
+      this.octagonCopyAssets()
+    ])
   },
   clean () {
     return Promise.all([
-      remove(this.tempBuildDir),
-      remove(this.coverageDir),
-      remove(this.distDir),
-      remove(this.styleguidistDist)
+      fs.remove(this.tempBuildDir),
+      fs.remove(this.coverageDir),
+      fs.remove(this.distDir),
+      fs.remove(this.styleguidistDist)
     ])
   },
   getBin (bin) {
@@ -60,20 +60,20 @@ module.exports = {
   },
   async octagonComponentJs (opts) {
     opts = opts || {}
-    const args = [this.getBin('babel'), 'src', '-d', this.componentDist, '--source-maps']
+    const cmd = this.getBin('babel')
+    const args = ['src', '-d', this.componentDist, '--source-maps']
     if (opts.watch) args.push('--watch')
-    await mkdirp(this.componentDist)
-    const [stdout] = await exec(args.join(' '), { cwd: this.projectRoot, stdio: 'inherit' })
-    console.log(stdout)
+    await fs.mkdirp(this.componentDist)
+    return execa(cmd, args, { cwd: this.projectRoot, stdio: 'inherit' })
   },
   async octagonComponentCss (opts) {
     opts = opts || {}
     const outputDir = path.join(this.componentDist, 'styles', 'components')
     const inputDir = path.join(this.projectRoot, 'src', 'styles', 'components', '*.css')
-    const args = [this.getBin('postcss'), inputDir, '-d', outputDir, '-c', this.postCssConfig]
-    await mkdirp(this.componentDist)
-    const [stdout] = await exec(args.join(' '), { cwd: this.projectRoot, stdio: 'inherit' })
-    console.log(stdout)
+    const cmd = this.getBin('postcss')
+    const args = [inputDir, '-d', outputDir, '-c', this.postCssConfig]
+    await fs.mkdirp(this.componentDist)
+    return execa(cmd, args, { cwd: this.projectRoot, stdio: 'inherit' })
   },
   async octagonCopyAssets (opts) {
     const assetSource = path.join(this.projectRoot, 'src', 'assets')
@@ -82,12 +82,13 @@ module.exports = {
     const latoDest = path.join(fontsDest, 'lato')
     const elegantSrc = path.resolve(this.projectRoot, 'node_modules', 'elegant-icons', 'fonts')
     const elegantDest = path.join(fontsDest, 'elegant-icons')
-    await copy(assetSource, this.assetsDist)
-    await copy(latoSrc, latoDest)
-    await copy(elegantSrc, elegantDest)
+    return Promise.all([
+      fs.copy(assetSource, this.assetsDist),
+      fs.copy(latoSrc, latoDest),
+      fs.copy(elegantSrc, elegantDest)
+    ])
   },
   async semanticBuild () {
-    console.log('Building Semantic Less')
     // pull these from semantic-ui-less node modules
     const definitionsPath = path.join(this.semanticUiLessPath, 'definitions')
     const definitionsDest = path.join(this.tempBuildDir, 'definitions')
@@ -100,32 +101,38 @@ module.exports = {
     // tw config and theme files
     const themeConfigPath = path.join(this.srcPath, 'theme.config')
     const themeConfigDest = path.join(this.tempBuildDir, 'theme.config')
+    const themeBuildPath = path.join(this.tempBuildDir, 'themes')
     const twThemePath = path.join(this.srcPath, 'themes', 'tripwire')
-    const twThemeDest = path.join(this.tempBuildDir, 'themes', 'tripwire')
+    const twThemeDest = path.join(themeBuildPath, 'tripwire')
 
-    // tw theme
-    await link(twThemePath, twThemeDest)
-    await copy(themeConfigPath, themeConfigDest) // hot reloading won't work for config
-    // semantic
-    await copy(themeLessPath, themeLessDest)
-    await copy(defaultThemePath, defaultThemeDest)
-    await copy(definitionsPath, definitionsDest)
-    await copy(semanticLessSrc, semanticLessPath)
+    await fs.mkdirp(themeBuildPath)
+    await Promise.all([
+      // tw theme
+      fs.symlink(twThemePath, twThemeDest),
+      fs.copy(themeConfigPath, themeConfigDest), // hot reloading won't work for config
+
+      // semantic
+      fs.copy(themeLessPath, themeLessDest),
+      fs.copy(defaultThemePath, defaultThemeDest),
+      fs.copy(definitionsPath, definitionsDest),
+      fs.copy(semanticLessSrc, semanticLessPath)
+    ])
     await this.semanticOverwriteStylePaths()
 
-    const lessInputStream = await fs.readFileSync(semanticLessPath).toString()
-    const outputPath = path.join(this.componentDist, 'styles', 'semantic.css')
+    const lessInputStream = (await fs.readFile(semanticLessPath)).toString()
+    const outputDir = path.join(this.componentDist, 'styles')
+    const outputPath = path.join(outputDir, 'semantic.css')
     const options = {filename: semanticLessPath}
     const output = await less.render(lessInputStream, options)
+    await fs.mkdirp(outputDir)
     return fs.writeFile(outputPath, output.css)
   },
   async semanticCopyAssets () {
-    console.log('Copying Semantic Assets')
     const assetsSource = path.join(this.tempBuildDir, 'themes', 'default', 'assets')
     const defaultDir = path.join(this.distDir, 'styles', 'themes', 'default')
     const assetsDest = path.join(defaultDir, 'assets')
-    await mkdirp(assetsDest)
-    await copy(assetsSource, assetsDest)
+    await fs.mkdirp(assetsDest)
+    return fs.copy(assetsSource, assetsDest)
   },
   async semanticInit () {
     // source maps not yet available!
@@ -134,10 +141,10 @@ module.exports = {
     return this.semanticCopyAssets()
   },
   async semanticOverwriteStylePaths () {
-    await mkdirp(path.join(this.tempBuildDir, 'site', 'globals'))
+    await fs.mkdirp(path.join(this.tempBuildDir, 'site', 'globals'))
     const siteVarsDest = path.join(this.tempBuildDir, 'site', 'globals', 'site.variables')
     const siteVarsText = '@imagePath : "/themes/default/assets/images";\n@fontPath  : "/themes/default/assets/fonts";'
-    fs.writeFile(siteVarsDest, siteVarsText)
+    return fs.writeFile(siteVarsDest, siteVarsText)
   },
   async styleguide () {
     await db.openAsync()
