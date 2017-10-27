44addb9327b4d6bf7738583e35890049d8895597
Andrew Leyva <aleyva@tripwire.com>
2017-10-27 13:50:32 -0700

    start.js just builds now. Watcher is less-loader

--- scripts/start.js
+++ scripts/start.js
@@ -6,38 +6,5 @@
 
 'use strict'
 
-const cp = require('child_process')
-const builder = require('./builder')
-
-function watchWatchCssTheme () {
-  const watchStylesProcess = cp.spawn(
-    builder.getBin('gulp'),
-    ['watch'],
-    { cwd: builder.semanticPath, stdio: 'inherit' }
-  )
-  // gulp generates assets periodically. hackishly just keep copying that
-  // gulp dist into our build target
-  setInterval(() => builder.copySemanticAssets(), 10000)
-
-  const styleguideProcess = cp.spawn(
-    'npm', ['run', 'styleguide'],
-    { cwd: builder.projectRoot, stdio: 'inherit' }
-  )
-
-  // kill both child processes on parent exit
-  ;[watchStylesProcess, styleguideProcess].forEach(p => {
-    p.on('exit', code => {
-      try {
-        p.kill()
-      } catch (err) {
-        /* pass */
-      }
-    })
-  })
-}
-
-
-
 builder.build()
-//TODO: fix watcher -> less-loader
-//.then(watchWatchCssTheme)
+// Watcher is now less-loader
