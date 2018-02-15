eb7402ce6adb882f0d1c19d4583ec18b65769e04
Andrew Leyva <aleyva@tripwire.com>
2018-02-15 12:35:36 -0800

    Added Stop-Start Button

--- src/components/native/StopStartButton/StopStartButton.jsx
+++ src/components/native/StopStartButton/StopStartButton.jsx
@@ -4,11 +4,11 @@ import Icon from '../../suir/icon/Icon'
 import '../../../styles/components/stop-start-button.css'
 
 const StopStartButton = (props) => {
-  let iconClass = ''
+  let iconClass = 'octagon'
   if (props.isStopped) {
-    iconClass = 'ei icon_pause_alt2 stop-start-button'
+    iconClass += ' ei icon_pause_alt2 stop-start-button'
   } else {
-    iconClass = 'ei arrow_triangle-right_alt2 stop-start-button'
+    iconClass += ' ei arrow_triangle-right_alt2 stop-start-button'
   }
   return (
     <Icon className={` ${iconClass} ${props.className}`} style={props.style} onClick={props.onClick} />
