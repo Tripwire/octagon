f5261966d7cb3ff3b08402b85fc1b555936bcc58
Andrew Leyva <aleyva@tripwire.com>
2018-02-23 17:56:56 -0800

    -mfeat(thincard): converted to stateless

--- src/components/native/ThinCard/ThinCard.jsx
+++ src/components/native/ThinCard/ThinCard.jsx
@@ -9,34 +9,32 @@ import ThinCardActionButton from './ThinCardActionButton'
 import ThinCardWidget from './ThinCardWidget'
 import ThinCardDrawer from './ThinCardDrawer'
 
-class ThinCard extends React.Component {
-  render () {
-    let borderClass = ''
-    if (this.props.noBorder) {
-      borderClass = 'thincard__noborder'
-    }
-    if (this.props.folder) {
-      borderClass += ' thincard__folderpad'
-    }
-    const externalAttributes = filterAttributesFromProps(this.props)
-    return (
-      <Flexbox
-        {...externalAttributes}
-        flexDirection='column'
-        className={`octagon thincard__row ${borderClass} ${this.props.className}`}
-        key={this.props.data.id}>
-
-        {(this.props.folder) ? (
-          <div className='topedge' />
-        ) : ''
-        }
-        <Flexbox flexDirection='row' flexGrow={3} className='primary-content'>
-          {this.props.children}
-        </Flexbox>
-        {this.props.drawer}
-      </Flexbox>
-    )
+const ThinCard = (props) => {
+  let borderClass = ''
+  if (props.noBorder) {
+    borderClass = 'thincard__noborder'
   }
+  if (props.folder) {
+    borderClass += ' thincard__folderpad'
+  }
+  const externalAttributes = filterAttributesFromProps(props)
+  return (
+    <Flexbox
+      {...externalAttributes}
+      flexDirection='column'
+      className={`octagon thincard__row ${borderClass} ${props.className}`}
+      key={props.data.id}>
+
+      {(props.folder) ? (
+        <div className='topedge' />
+      ) : ''
+      }
+      <Flexbox flexDirection='row' flexGrow={3} className='primary-content'>
+        {props.children}
+      </Flexbox>
+      {props.drawer}
+    </Flexbox>
+  )
 }
 ThinCard.Title = ThinCardTitle
 ThinCard.PrimaryAction = ThinCardPrimaryAction
