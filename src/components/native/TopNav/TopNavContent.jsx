585399b592cbdbed56091560f89bef2733e30076
cdaringe <christopher.dieringer@gmail.com>
2018-04-27 12:30:19 -0700

    fix(TopNav): accept handled props on root nodes

--- src/components/native/TopNav/TopNavContent.jsx
+++ src/components/native/TopNav/TopNavContent.jsx
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import externalAttributeFilter from '../../../util/externalAttributeFilter'
+import classnames from 'classnames'
 
 const TopNavContent = (props) => {
+  var handledProps = externalAttributeFilter(props)
   return (
-    <div className={`nav__main--${props.align}`}>
+    <div
+      {...handledProps}
+      className={classnames(`nav__main--${props.align}`, handledProps.className)}>
       <div className={`nav__item ` + ((props.align === 'right') ? 'nav__link' : '')}>
         {props.children}
       </div>
