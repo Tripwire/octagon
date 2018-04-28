2ade22f322e6f9e1a654a9ebbb1804ac6ea75524
cdaringe <christopher.dieringer@gmail.com>
2018-04-27 18:43:32 -0700

    fix(LargeCard): document Gutter

--- src/components/native/LargeCard/LargeCardGutter.jsx
+++ src/components/native/LargeCard/LargeCardGutter.jsx
@@ -1,22 +1,29 @@
 import Flexbox from 'flexbox-react'
 import React from 'react'
 import palette from '../../../palette'
+import PropTypes from 'prop-types'
 import filterAttributesFromProps from '../../../util/externalAttributeFilter'
 
+const STATE_COLOR_MAP = {
+  CRITICAL: 'critical',
+  WARNING: 'warning',
+  NORMAL: 'normal',
+  BLUE: 'blue' // @deprecated
+}
 const LargeCardGutter = (props) => {
   const externalAttributes = filterAttributesFromProps(props)
   let color = ''
   switch (props.color) {
-    case 'critical':
+    case STATE_COLOR_MAP.CRITICAL:
       color = palette.red
       break
-    case 'warning':
+    case STATE_COLOR_MAP.WARNING:
       color = palette.yellow
       break
-    case 'normal':
+    case STATE_COLOR_MAP.NORMAL:
       color = palette.blue
       break
-    case 'blue':
+    case STATE_COLOR_MAP.BLUE:
       color = palette.blue
       break
     default:
@@ -25,7 +32,7 @@ const LargeCardGutter = (props) => {
   return (
     <Flexbox
       {...externalAttributes}
-      className={`fullview__left_border ${props.className}`}
+      className={`fullview__left_border ${props.className || ''}`}
       width='9px'
       style={{ backgroundColor: color }} />
   )
@@ -36,7 +43,13 @@ LargeCardGutter.defaultProps = {
 }
 
 LargeCardGutter.propTypes = {
-
+  /** color of gutter. use `LargeCardGutter.STATE_COLOR_MAP` or any standard web color string  */
+  color: PropTypes.oneOf(
+    Object.values(STATE_COLOR_MAP).concat(
+      PropTypes.string
+    )
+  )
 }
+LargeCardGutter.STATE_COLOR_MAP = STATE_COLOR_MAP
 
 export default LargeCardGutter
