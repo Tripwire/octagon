5470b6b9d8daeed2e5778634f1fa025cd44caf87
Andrew Leyva <andrew.leyva@gmail.com>
2018-12-17 16:54:48 -0800

    added some proptypes

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,4 +1,5 @@
 import '../../styles/components/notification.css'
+import PropTypes from 'prop-types'
 import React from 'react'
 import { Popup } from 'semantic-ui-react'
 import NotificationBadge from './NotificationBadge'
@@ -70,5 +71,38 @@ Notification.Header = NotificationHeader
 Notification.Body = NotificationBody
 Notification.Footer = NotificationFooter
 Notification.Empty = NotificationEmpty
+
+Notification.propTypes = {
+  /**
+   * Called when a notification message is clicked. Opens
+   * Detail View and displays notification message.
+   * @param {Object} notification message
+   */
+  onNotificationClicked: PropTypes.func.isRequired.isRequired,
+  /**
+   * Called when a back button is clicked. Returns to
+   * List View.
+   */
+  onBackClicked: PropTypes.func.isRequired,
+  /**
+   * Called when a Clear Notification button is clicked. Clears
+   * Notication Message from List View and archives it.
+   * @param {Object} notification message
+   */
+  onClearNotification: PropTypes.func.isRequired,
+  /**
+   * Called when a Clear All button is clicked. Clears all
+   * Notication Message from List View and archives all.
+   */
+  onClearAll: PropTypes.func.isRequired,
+  /**
+   * Set selected notification to view.
+   */
+  selectedNotification: PropTypes.string,
+  /**
+   * Set notification data.
+   */
+  notifications: PropTypes.object.isRequired
+}
 
 export default Notification
