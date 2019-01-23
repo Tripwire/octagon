196cdb8f90b2110e410bc7931e1d69bd574d7858
gsarbhukan <gsarbhukan@tripwire.com>
2019-01-23 14:43:53 +0530

    feat(button-component): changes for button component as per code review

--- src/components/RolesActionList/RolesActionList.jsx
+++ src/components/RolesActionList/RolesActionList.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { ActionListButton } from '../ActionListButton/ActionListButton'
 import { Menu, Popup } from 'semantic-ui-react'
+import classNames from 'classnames'
 
 const RolesActionList = props => {
   const { disabled } = props
+  const btnClass = classNames('action-list', { disabled: disabled })
 
   return (
     <Popup
-      className={`action-list ${disabled ? 'disabled' : null}`}
+      className={btnClass}
       on='click'
       hoverable
       closeOnTriggerMouseLeave
@@ -16,17 +18,16 @@ const RolesActionList = props => {
       style={{ padding: 0 }}
       trigger={
         <span>
-          {' '}
           <ActionListButton {...props} />
         </span>
       }
     >
-      {!disabled ? (
+      {!disabled && (
         <Menu vertical>
           <Menu.Item name='edit'>Export</Menu.Item>
           <Menu.Item name='delete'>Delete</Menu.Item>
         </Menu>
-      ) : null}
+      )}
     </Popup>
   )
 }
